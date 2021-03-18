6e7eb59ad54a05339dafe260b9fb782ff2e45f2e
datacantech <44164636+datacantech@users.noreply.github.com>
2021-03-18 23:04:54 +0300

    Create sc.groovy

--- sc.groovy
+++ sc.groovy
@@ -1,3 +1,4 @@
+//delete
 item = Jenkins.instance.getItemByFullName("job-name")
 item.builds.each() { build ->
   build.delete()
@@ -13,7 +14,21 @@ Jenkins.instance.allItems(hudson.model.AbstractProject.class).each {it ->
     }
 }
 return
-//
+//get
 Jenkins.instance.allItems.findAll {
   it instanceof hudson.model.AbstractProject
 }.each
+//makeDisabled
+Jenkins.instance.getAllItems(hudson.model.AbstractProject.class).each {it ->
+  scm = it.getScm()
+  if(scm instanceof hudson.plugins.git.GitSCM)
+  {
+     if(scm.getUserRemoteConfigs()[0].getUrl().contains('XXXXXXXX'))
+     {
+        println scm.getUserRemoteConfigs()[0].getUrl()
+        println it.name
+        it.makeDisabled(true)
+      }
+  }
+}
+println "Done"
