64b23108ee3aceafc35230644ca63864579cdeea
datacantech <44164636+datacantech@users.noreply.github.com>
2021-03-19 00:00:45 +0300

    Update sc.groovy

--- sc.groovy
+++ sc.groovy
@@ -14,6 +14,30 @@ Jenkins.instance.allItems(hudson.model.AbstractProject.class).each {it ->
     }
 }
 return
+//
+import hudson.model.*
+def pipe_view = "Pipeline_folder"
+def view = Hudson.instance.getView(pipe_view)
+def successfulJobs = view.getItems().findAll{job -> job.lastBuild != null && job.lastBuild.result == hudson.model.Result.SUCCESS}
+def faildJobs = view.getItems().findAll{job -> job.lastBuild != null && job.lastBuild.result == hudson.model.Result.FAILURE}
+def disabledJob = view.getItems().findAll{job -> job.disabled == true}
+def enabledJob = view.getItems().findAll{job -> job.disabled != true}
+println "Total jobs: " + view.getItems().size +" Successful: " +successfulJobs.size+
+  " Failed: " + faildJobs.size + " Enabled jobs: " +enabledJob.size + " Disabled jobs: " +disabledJob.size 
+
+println "Current Successful job:"
+successfulJobs.each{job -> printInfo(job)}
+println "Current Fail job:"
+faildJobs.each{job -> printInfo(job)}
+println "Current disabled job:"
+disabledJob.each{job -> printInfo(job)}
+println "Current enabled job:"
+enabledJob.each{job -> printInfo(job)}
+
+def printInfo(job){
+  println "Job: ${job.name} build on ${job.getAssignedLabelString()}, "+
+    "took ${job.lastBuild.getDurationString()} to build, is disabled : ${job.disabled}"
+}
 //get
 Jenkins.instance.allItems.findAll {
   it instanceof hudson.model.AbstractProject
